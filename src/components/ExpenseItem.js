a898428f7ba1910520a2cd12b884fbf8f11eab6b
Nitesh <shuklanitesh805@gmail.com>
2023-04-29 21:01:07 +0530

    use props and build components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem(){
-    return <h2> ExpenseItem!!</h2>
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem(props){
+
+    
+    return (
+    <div className="expense-item">
+        <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+    </div>
+    );
+
 }
 
 export default ExpenseItem;
